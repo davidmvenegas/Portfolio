66c494d5646648fce3265df9889c2ec974d240d0
david_venegas <venegasdavidm@gmail.com>
2022-02-02 01:13:58 -0500

    html for skills

--- src/2-Skills/Skills.js
+++ src/2-Skills/Skills.js
@@ -5,6 +5,9 @@ function Skills() {
         <div className='skills-container'>
             <div className="skills-container">
                 <h1 className="skills-title">My Proficiencies</h1>
+                <p className="skills-description">
+                    I love to learn and am always teaching myself new things. These are my strongest skillsets and technologies.
+                </p>
                 <div className="skills-front-box">
                     <h1 className="skills-front-title">Frontend</h1>
                     <div className="skills-front-content">
@@ -56,19 +59,51 @@ function Skills() {
                         <div className="skills-back-item">
                             <img src="" alt="SQL" />
                             <h2>SQL</h2>
-                            <p>I am well versed in SQL development, from queries to schemas</p>
+                            <p>I am well versed in SQL development - from queries to schemas</p>
                         </div>
                         <div className="skills-back-item">
                             <img src="" alt="Firebase" />
                             <h2>Firebase</h2>
-                            <p>A highly versitile NoSQL database - I'm also experienced in its Storage and Authentication systems</p>
+                            <p>A highly versatile NoSQL database - I'm also experienced in its Storage and Authentication systems</p>
                         </div>
                     </div>
                 </div>
             </div>
             <div className="education-container">
                 <h1 className="education-title">Education and Certifications</h1>
-                
+                <div className="education-wrapper">
+                    <div className="education-item">
+                        <img src="" alt="AWS" />
+                        <div className="education-item-text">
+                            <h1>AMAZON WEB SERVICES</h1>
+                            <h4>Certified Cloud Practitioner</h4>
+                            <p>The foundational accreditation from the world's largest cloud provider. Cloud technology is expanding at an incredible pace and I intend to receive many more qualifications for the field in the near future.</p>
+                        </div>
+                    </div>
+                    <div className="education-item">
+                        <div className="education-img-box">
+                            <img src="" alt="Flatiron" />
+                            <p>From Oct 2021 to Feb 2022</p>
+                        </div>
+                        <div className="education-item-text">
+                            <h3>NYC, New York City</h3>
+                            <h1>FLATIRON SCHOOL</h1>
+                            <h4>Software Engineering - 16 weeks - Full Time & In-person</h4>
+                            <p>A highly acknowledged Software Engineering trade school and proven leader in tech education. During my time as a student I studied coding best practices, engaged in pair programming with my peers, collaborated on projects as a team and learned proper ways to tackle complex problems. Throughout my time at the school I actively improved my skills with 500+ hours of live coding experience.</p>
+                        </div>
+                    </div>
+                    <div className="education-item">
+                        <img src="" alt="Flatiron" />
+                        <div className="education-item-text">
+                            <h1>CYBER SECURITY</h1>
+                            <h4>CompTiA Security +</h4>
+                            <p>A globally recognized certification that validates the baseline skills necessary to perform core security functions. Cybersecurity is a core component of the internet and I'm looking forward to expanding my knowledge further.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="note-container">
+                <p><span>*</span> While I don't have a computer science degree, I do have a working understanding of the theory and maths behind the code. I've completed Harvard's CS50, finished numerous of MIT's Computer Science courses, read countless textbooks, taken advanced math courses and much more. I'm passionate about the fundamentals of programming - not just at the surface level, and I believe that dedication will show.</p>
             </div>
         </div>
     )
