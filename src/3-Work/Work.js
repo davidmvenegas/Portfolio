efba6822aa2fe6c2c594436294201c98ea03277f
david_venegas <venegasdavidm@gmail.com>
2022-02-03 16:43:44 -0500

    projects layout looks good

--- src/3-Work/Work.js
+++ src/3-Work/Work.js
@@ -1,5 +1,6 @@
 import './work.css'
 import { useNavigate } from 'react-router-dom'
+import { BsArrowRight } from 'react-icons/bs'
 
 function Work() {
     const navigate = useNavigate()
@@ -26,99 +27,82 @@ function Work() {
                         </div>
                     </p>
                     <div className="port-items">
-                        <div className="port-items-t">
-                            <div className="port-item-t">
-                                <div className="port-gif-box-t">
-                                    <img className='port-gif-t' src="" alt="Grocery Magix Gif" />
-                                    <div className="portGifPopup-t">
-                                        <p>Visit the site</p>
-                                    </div>
-                                </div>
-                                <div className="port-text-box-t">
-                                    <h1 className="port-title-t">Grocery Magix<span>&trade;</span></h1>
-                                    <a className="port-title-site-t" href='https://www.grocerymagix.com/' target="_blank" rel="noreferrer">www.grocerymagix.com</a>
-                                    <p className="port-headline-t">Search through millions of recipes, pick your favorites and instantly generate a shopping list.</p>
-                                    <div className="port-btn-box-t">
-                                        <button className="port-btn-visit-t">Visit Site</button>
-                                        <button className="port-btn-more-t">More Details</button>
-                                    </div>
+                        <div className="port-item">
+                            <div className="port-gif-box">
+                                <img className='port-gif' src="" alt="My Simple Wardrobe Gif" />
+                                <div className="portGifPopup">
+                                    <p>Visit the site</p>
                                 </div>
                             </div>
-                            <div className="port-item-t">
-                                <div className="port-gif-box-t">
-                                    <img className='port-gif-t' src="" alt="Grocery Magix Gif" />
-                                    <div className="portGifPopup-t">
-                                        <p>Visit the site</p>
-                                    </div>
-                                </div>
-                                <div className="port-text-box-t">
-                                    <h1 className="port-title-t">Grocery Magix<span>&trade;</span></h1>
-                                    <a className="port-title-site-t" href='https://www.grocerymagix.com/' target="_blank" rel="noreferrer">www.grocerymagix.com</a>
-                                    <p className="port-headline-t">Search through millions of recipes, pick your favorites and instantly generate a shopping list.</p>
-                                    <div className="port-btn-box-t">
-                                        <button className="port-btn-visit-t">Visit Site</button>
-                                        <button className="port-btn-more-t">More Details</button>
+                            <div className="port-text-box">
+                                <h1 className="port-title">My Simple Wardrobe<span>&trade;</span></h1>
+                                <a className="port-title-site" href='https://www.mysimplewardrobe.com/' target="_blank" rel="noreferrer">www.mysimplewardrobe.com</a>
+                                <p className="port-headline">A fully functional eCommerce clothing store with Stripe&trade; checkout and an Admin panel</p>
+                                <div className="port-btn-box">
+                                    <div className='port-btn-more'>
+                                        <span id='port-more-text'>More Details</span>
+                                        <BsArrowRight id='port-more-arrow'/>
                                     </div>
                                 </div>
                             </div>
                         </div>
-
-
-                        {/* <div className="port-items-b">
-                            <div className="port-item-b">
-                                <div className="port-gif-box-b">
-                                    <img className='port-gif-b' src="" alt="Grocery Magix Gif" />
-                                    <div className="portGifPopup-b">
-                                        <p>Visit the site</p>
-                                    </div>
+                        <div className="port-item">
+                            <div className="port-gif-box">
+                                <img className='port-gif' src="" alt="Grocery Magix Gif" />
+                                <div className="portGifPopup">
+                                    <p>Visit the site</p>
                                 </div>
-                                <div className="port-text-box-b">
-                                    <h1 className="port-title-b">Grocery Magix<span>&trade;</span></h1>
-                                    <a className="port-title-site-b" href='https://www.grocerymagix.com/' target="_blank" rel="noreferrer">www.grocerymagix.com</a>
-                                    <p className="port-headline-b">Search through millions of recipes, pick your favorites and instantly generate a shopping list.</p>
-                                    <div className="port-btn-box-b">
-                                        <button className="port-btn-visit-b">Visit Site</button>
-                                        <button className="port-btn-more-b">More Details</button>
+                            </div>
+                            <div className="port-text-box">
+                                <h1 className="port-title">Grocery Magix<span>&trade;</span></h1>
+                                <a className="port-title-site" href='https://www.grocerymagix.com/' target="_blank" rel="noreferrer">www.grocerymagix.com</a>
+                                <p className="port-headline">Search through millions of recipes, pick your favorites and instantly generate a shopping list.</p>
+                                <div className="port-btn-box">
+                                    <div className='port-btn-more'>
+                                        <span id='port-more-text'>More Details</span>
+                                        <BsArrowRight id='port-more-arrow'/>
                                     </div>
                                 </div>
                             </div>
-                            <div className="port-item-b">
-                                <div className="port-gif-box-b">
-                                    <img className='port-gif-b' src="" alt="Grocery Magix Gif" />
-                                    <div className="portGifPopup-b">
-                                        <p>Visit the site</p>
-                                    </div>
+                        </div>
+                        <div className="port-item">
+                            <div className="port-gif-box">
+                                <img className='port-gif' src="" alt="The Financial Tracker Gif" />
+                                <div className="portGifPopup">
+                                    <p>Visit the site</p>
                                 </div>
-                                <div className="port-text-box-b">
-                                    <h1 className="port-title-b">Grocery Magix<span>&trade;</span></h1>
-                                    <a className="port-title-site-b" href='https://www.grocerymagix.com/' target="_blank" rel="noreferrer">www.grocerymagix.com</a>
-                                    <p className="port-headline-b">Search through millions of recipes, pick your favorites and instantly generate a shopping list.</p>
-                                    <div className="port-btn-box-b">
-                                        <button className="port-btn-visit-b">Visit Site</button>
-                                        <button className="port-btn-more-b">More Details</button>
+                            </div>
+                            <div className="port-text-box">
+                                <h1 className="port-title">The Financial Tracker<span>&trade;</span></h1>
+                                <a className="port-title-site" href='https://www.thefinancialtracker.com/' target="_blank" rel="noreferrer">www.thefinancialtracker.com</a>
+                                <p className="port-headline">Track your income, set a budget and visualize your saving goals with this personal finance tracker</p>
+                                <div className="port-btn-box">
+                                    <div className='port-btn-more'>
+                                        <span id='port-more-text'>More Details</span>
+                                        <BsArrowRight id='port-more-arrow'/>
                                     </div>
                                 </div>
                             </div>
-                            <div className="port-item-b">
-                                <div className="port-gif-box-b">
-                                    <img className='port-gif-b' src="" alt="Grocery Magix Gif" />
-                                    <div className="portGifPopup-b">
-                                        <p>Visit the site</p>
-                                    </div>
+                        </div>
+                        <div className="port-item">
+                            <div className="port-gif-box">
+                                <img className='port-gif' src="" alt="Grocery Magix Gif" />
+                                <div className="portGifPopup">
+                                    <p>Visit the site</p>
                                 </div>
-                                <div className="port-text-box-b">
-                                    <h1 className="port-title-b">Grocery Magix<span>&trade;</span></h1>
-                                    <a className="port-title-site-b" href='https://www.grocerymagix.com/' target="_blank" rel="noreferrer">www.grocerymagix.com</a>
-                                    <p className="port-headline-b">Search through millions of recipes, pick your favorites and instantly generate a shopping list.</p>
-                                    <div className="port-btn-box-b">
-                                        <button className="port-btn-visit-b">Visit Site</button>
-                                        <button className="port-btn-more-b">More Details</button>
+                            </div>
+                            <div className="port-text-box">
+                                <h1 className="port-title">Cryptomatic<span>&trade;</span></h1>
+                                <a className="port-title-site" href='https://www.mycryptomatic.com/' target="_blank" rel="noreferrer">www.mycryptomatic.com</a>
+                                <p className="port-headline">A cryptocurrency tracker with a view of the markets, watchlist, portfolio calculator and news</p>
+                                <div className="port-btn-box">
+                                    <div className='port-btn-more'>
+                                        <span id='port-more-text'>More Details</span>
+                                        <BsArrowRight id='port-more-arrow'/>
                                     </div>
                                 </div>
                             </div>
-                        </div> */}
-
-
+                        </div>
                     </div>
                 </div>
             <div className="other-work-wrapper"></div>
