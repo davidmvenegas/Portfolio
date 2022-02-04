787f6edba84c09c7f0e72f876b444ae58be98451
david_venegas <venegasdavidm@gmail.com>
2022-02-04 11:00:09 -0500

    other work animation complete

--- src/3-Work/Work.js
+++ src/3-Work/Work.js
@@ -34,117 +34,151 @@ function Work() {
                         <span>l</span>
                         <span>i</span>
                         <span>o</span>
-                    </h1>
-                    <p className="port-description">
-                        <div id='port-slide1'>A gallery of recent projects I've built.&nbsp;&nbsp;</div> 
-                        <div id='port-slide2'> 
-                            <span id='port-contact-slide'><span onClick={() => navigate('/contact')}>Contact me</span> for a complete code walkthrough.</span>
+                </h1>
+                <p className="port-description">
+                    <div id='port-slide1'>A gallery of recent projects I've built.&nbsp;&nbsp;</div> 
+                    <div id='port-slide2'> 
+                        <span id='port-contact-slide'><span onClick={() => navigate('/contact')}>Contact me</span> for a complete code walkthrough.</span>
+                    </div>
+                </p>
+                <div className="port-items">
+                    <div className="port-item" onMouseEnter={() => setPort1(true)} onMouseLeave={() => setPort1(false)}>
+                        <div className="port-gif-box">
+                            <div className="port-gif-wrapper">
+                                <img className='port-gif' src={MySimpleWardrobeGif} alt="My Simple Wardrobe Gif" style={port1 ? {opacity: "1"} : {opacity: "0"}}/>
+                                <img className='port-gif' src={MySimpleWardrobeLogo} alt="My Simple Wardrobe Logo" style={port1 ? {opacity: "0"} : {opacity: "1"}}/>
+                            </div>
+                            <a href='https://www.mysimplewardrobe.com/' target="_blank" rel="noreferrer">
+                                <div className="portGifPopup">
+                                    <p>Go to site</p>
+                                    <BsBoxArrowUpRight id='goToSitePort'/>
+                                </div>
+                            </a>
                         </div>
-                    </p>
-                    <div className="port-items">
-                        <div className="port-item" onMouseEnter={() => setPort1(true)} onMouseLeave={() => setPort1(false)}>
-                            <div className="port-gif-box">
-                                <div className="port-gif-wrapper">
-                                    <img className='port-gif' src={MySimpleWardrobeGif} alt="My Simple Wardrobe Gif" style={port1 ? {opacity: "1"} : {opacity: "0"}}/>
-                                    <img className='port-gif' src={MySimpleWardrobeLogo} alt="My Simple Wardrobe Logo" style={port1 ? {opacity: "0"} : {opacity: "1"}}/>
-                                </div>
-                                <a href='https://www.mysimplewardrobe.com/' target="_blank" rel="noreferrer">
-                                    <div className="portGifPopup">
-                                        <p>Go to site</p>
-                                        <BsBoxArrowUpRight id='goToSitePort'/>
-                                    </div>
-                                </a>
-                            </div>
-                            <div className="port-text-box">
-                                <h1 className="port-title">My Simple Wardrobe<span>&trade;</span></h1>
-                                <a className="port-title-site" href='https://www.mysimplewardrobe.com/' target="_blank" rel="noreferrer">www.mysimplewardrobe.com</a>
-                                <p className="port-headline">A fully functional eCommerce clothing store with Stripe&trade; checkout and an Admin panel</p>
-                                <div className="port-btn-box">
-                                    <div className='port-btn-more'>
-                                        <span id='port-more-text'>More Details</span>
-                                        <BsArrowRight id='port-more-arrow'/>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="port-item" onMouseEnter={() => setPort2(true)} onMouseLeave={() => setPort2(false)}>
-                            <div className="port-gif-box">
-                                <div className="port-gif-wrapper">
-                                    <img className='port-gif' src={GroceryMagixGif} alt="Grocery Magix Gif" style={port2 ? {opacity: "1"} : {opacity: "0"}}/>
-                                    <img className='port-gif' src={GroceryMagixLogo} alt="Grocery Magix Logo" style={port2 ? {opacity: "0"} : {opacity: "1"}}/>
-                                </div>
-                                <a href='https://www.grocerymagix.com/' target="_blank" rel="noreferrer">
-                                    <div className="portGifPopup">
-                                        <p>Go to site</p>
-                                        <BsBoxArrowUpRight id='goToSitePort'/>
-                                    </div>
-                                </a>
-                            </div>
-                            <div className="port-text-box">
-                                <h1 className="port-title">Grocery Magix<span>&trade;</span></h1>
-                                <a className="port-title-site" href='https://www.grocerymagix.com/' target="_blank" rel="noreferrer">www.grocerymagix.com</a>
-                                <p className="port-headline">Search through millions of recipes, pick your favorites and instantly generate a shopping list.</p>
-                                <div className="port-btn-box">
-                                    <div className='port-btn-more'>
-                                        <span id='port-more-text'>More Details</span>
-                                        <BsArrowRight id='port-more-arrow'/>
-                                    </div>
+                        <div className="port-text-box">
+                            <h1 className="port-title">My Simple Wardrobe<span>&trade;</span></h1>
+                            <a className="port-title-site" href='https://www.mysimplewardrobe.com/' target="_blank" rel="noreferrer">www.mysimplewardrobe.com</a>
+                            <p className="port-headline">A fully functional eCommerce clothing store with Stripe&trade; checkout and an Admin panel.</p>
+                            <div className="port-btn-box">
+                                <div className='port-btn-more'>
+                                    <span id='port-more-text'>More Details</span>
+                                    <BsArrowRight id='port-more-arrow'/>
                                 </div>
                             </div>
                         </div>
-                        <div className="port-item" onMouseEnter={() => setPort3(true)} onMouseLeave={() => setPort3(false)}>
-                            <div className="port-gif-box">
-                                <div className="port-gif-wrapper">
-                                    <img className='port-gif' src={TheFinancialTrackerGif} alt="The Financial Tracker Gif" style={port3 ? {opacity: "1"} : {opacity: "0"}}/>
-                                    <img className='port-gif' src={TheFinancialTrackerLogo} alt="The Financial Tracker Logo" style={port3 ? {opacity: "0"} : {opacity: "1"}}/>
-                                </div>
-                                <a href='https://www.thefinancialtracker.com/' target="_blank" rel="noreferrer">
-                                    <div className="portGifPopup">
-                                        <p>Go to site</p>
-                                        <BsBoxArrowUpRight id='goToSitePort'/>
-                                    </div>
-                                </a>
+                    </div>
+                    <div className="port-item" onMouseEnter={() => setPort2(true)} onMouseLeave={() => setPort2(false)}>
+                        <div className="port-gif-box">
+                            <div className="port-gif-wrapper">
+                                <img className='port-gif' src={GroceryMagixGif} alt="Grocery Magix Gif" style={port2 ? {opacity: "1"} : {opacity: "0"}}/>
+                                <img className='port-gif' src={GroceryMagixLogo} alt="Grocery Magix Logo" style={port2 ? {opacity: "0"} : {opacity: "1"}}/>
                             </div>
-                            <div className="port-text-box">
-                                <h1 className="port-title">The Financial Tracker<span>&trade;</span></h1>
-                                <a className="port-title-site" href='https://www.thefinancialtracker.com/' target="_blank" rel="noreferrer">www.thefinancialtracker.com</a>
-                                <p className="port-headline">Track your income, set a budget and visualize your saving goals with this personal finance tracker</p>
-                                <div className="port-btn-box">
-                                    <div className='port-btn-more'>
-                                        <span id='port-more-text'>More Details</span>
-                                        <BsArrowRight id='port-more-arrow'/>
-                                    </div>
+                            <a href='https://www.grocerymagix.com/' target="_blank" rel="noreferrer">
+                                <div className="portGifPopup">
+                                    <p>Go to site</p>
+                                    <BsBoxArrowUpRight id='goToSitePort'/>
+                                </div>
+                            </a>
+                        </div>
+                        <div className="port-text-box">
+                            <h1 className="port-title">Grocery Magix<span>&trade;</span></h1>
+                            <a className="port-title-site" href='https://www.grocerymagix.com/' target="_blank" rel="noreferrer">www.grocerymagix.com</a>
+                            <p className="port-headline">Search through millions of recipes, pick your favorites and instantly generate a shopping list.</p>
+                            <div className="port-btn-box">
+                                <div className='port-btn-more'>
+                                    <span id='port-more-text'>More Details</span>
+                                    <BsArrowRight id='port-more-arrow'/>
                                 </div>
                             </div>
                         </div>
-                        <div className="port-item" onMouseEnter={() => setPort4(true)} onMouseLeave={() => setPort4(false)}>
-                            <div className="port-gif-box">
-                                <div className="port-gif-wrapper">
-                                    <img className='port-gif' src={MyCryptomaticGif} alt="Grocery Magix Gif" style={port4 ? {opacity: "1"} : {opacity: "0"}}/>
-                                    <img className='port-gif' src={MyCryptomaticLogo} alt="Grocery Magix Logo" style={port4 ? {opacity: "0"} : {opacity: "1"}}/>
-                                </div>
-                                <a href='https://www.mycryptomatic.com/' target="_blank" rel="noreferrer">
-                                    <div className="portGifPopup">
-                                        <p>Go to site</p>
-                                        <BsBoxArrowUpRight id='goToSitePort'/>
-                                    </div>
-                                </a>
+                    </div>
+                    <div className="port-item" onMouseEnter={() => setPort3(true)} onMouseLeave={() => setPort3(false)}>
+                        <div className="port-gif-box">
+                            <div className="port-gif-wrapper">
+                                <img className='port-gif' src={TheFinancialTrackerGif} alt="The Financial Tracker Gif" style={port3 ? {opacity: "1"} : {opacity: "0"}}/>
+                                <img className='port-gif' src={TheFinancialTrackerLogo} alt="The Financial Tracker Logo" style={port3 ? {opacity: "0"} : {opacity: "1"}}/>
                             </div>
-                            <div className="port-text-box">
-                                <h1 className="port-title">My Cryptomatic<span>&trade;</span></h1>
-                                <a className="port-title-site" href='https://www.mycryptomatic.com/' target="_blank" rel="noreferrer">www.mycryptomatic.com</a>
-                                <p className="port-headline">A cryptocurrency tracker with a view of the markets, watchlist, portfolio calculator and news</p>
-                                <div className="port-btn-box">
-                                    <div className='port-btn-more'>
-                                        <span id='port-more-text'>More Details</span>
-                                        <BsArrowRight id='port-more-arrow'/>
-                                    </div>
+                            <a href='https://www.thefinancialtracker.com/' target="_blank" rel="noreferrer">
+                                <div className="portGifPopup">
+                                    <p>Go to site</p>
+                                    <BsBoxArrowUpRight id='goToSitePort'/>
+                                </div>
+                            </a>
+                        </div>
+                        <div className="port-text-box">
+                            <h1 className="port-title">The Financial Tracker<span>&trade;</span></h1>
+                            <a className="port-title-site" href='https://www.thefinancialtracker.com/' target="_blank" rel="noreferrer">www.thefinancialtracker.com</a>
+                            <p className="port-headline">Track your income, set a budget and visualize your saving goals with this personal finance tracker.</p>
+                            <div className="port-btn-box">
+                                <div className='port-btn-more'>
+                                    <span id='port-more-text'>More Details</span>
+                                    <BsArrowRight id='port-more-arrow'/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="port-item" onMouseEnter={() => setPort4(true)} onMouseLeave={() => setPort4(false)}>
+                        <div className="port-gif-box">
+                            <div className="port-gif-wrapper">
+                                <img className='port-gif' src={MyCryptomaticGif} alt="Grocery Magix Gif" style={port4 ? {opacity: "1"} : {opacity: "0"}}/>
+                                <img className='port-gif' src={MyCryptomaticLogo} alt="Grocery Magix Logo" style={port4 ? {opacity: "0"} : {opacity: "1"}}/>
+                            </div>
+                            <a href='https://www.mycryptomatic.com/' target="_blank" rel="noreferrer">
+                                <div className="portGifPopup">
+                                    <p>Go to site</p>
+                                    <BsBoxArrowUpRight id='goToSitePort'/>
+                                </div>
+                            </a>
+                        </div>
+                        <div className="port-text-box">
+                            <h1 className="port-title">My Cryptomatic<span>&trade;</span></h1>
+                            <a className="port-title-site" href='https://www.mycryptomatic.com/' target="_blank" rel="noreferrer">www.mycryptomatic.com</a>
+                            <p className="port-headline">A cryptocurrency tracker with a view of the markets, watchlist, portfolio calculator and news.</p>
+                            <div className="port-btn-box">
+                                <div className='port-btn-more'>
+                                    <span id='port-more-text'>More Details</span>
+                                    <BsArrowRight id='port-more-arrow'/>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            <div className="other-work-wrapper"></div>
+            </div>
+            <div className="other-separator"></div>
+            <div className="other-wrapper">
+                <h1 className="other-header">
+                        <span>O</span>
+                        <span>t</span>
+                        <span>h</span>
+                        <span>e</span>
+                        <span>r</span>&nbsp;
+                        <span>W</span>
+                        <span>o</span>
+                        <span>r</span>
+                        <span>k</span>
+                </h1>
+                <p className="other-description">
+                    A collection of some other work I've done. Everything is fully mobile responsive.
+                </p>
+                <div className="other-items">
+
+
+
+
+                    <div className="other-item">
+                        <div className='other-item-top'>
+                            <img src="https://i.pravatar.cc/300" alt='fdiuk'/>
+                            <h1>John Doe</h1>
+                        </div>
+                        <div className="other-item-bottom">
+                            <p>Terence is an avid reader of science fiction, especially anything to do with aliens and tweed jackets. Most weekends, he can be found cataloguing his collection of rodent skeletons.</p>
+                        </div>
+                    </div>  
+
+
+
+                </div>
+            </div>
         </div>
     )
 }
