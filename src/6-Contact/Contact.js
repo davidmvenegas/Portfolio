4f6fa6bfacbbfd5d617d02c49e987580babc192d
david_venegas <venegasdavidm@gmail.com>
2022-02-07 08:05:18 -0500

    added map

--- src/6-Contact/Contact.js
+++ src/6-Contact/Contact.js
@@ -5,7 +5,18 @@ function Contact() {
     useEffect(() => {window.scrollTo(0, 0)}, [])
     return (
         <div className='contact-container'>
-            CONTACT
+            <div className="contact-left-box">
+                <div className="contact-header"></div>
+                <div className="contact-message"></div>
+                <div className="contact-form"></div>
+                <div className="contact-aside"></div>
+            </div>
+            <div className="contact-right-box">
+                <div className="map-popup"></div>
+                <div className="map-wrapper">
+                    <iframe src="https://snazzymaps.com/embed/370475" width="550px" height="755px" style={{border: 'none'}} title='Contact Map'></iframe>
+                </div>
+            </div>
         </div>
     )
 }
