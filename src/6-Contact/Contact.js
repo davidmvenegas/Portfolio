c845fd5b005693f82bc3534c059bd0da0c00d607
david_venegas <venegasdavidm@gmail.com>
2022-02-07 22:49:19 -0500

    map done, form in progress

--- src/6-Contact/Contact.js
+++ src/6-Contact/Contact.js
@@ -6,14 +6,34 @@ function Contact() {
     return (
         <div className='contact-container'>
             <div className="contact-left-box">
-                <div className="contact-header"></div>
-                <div className="contact-message"></div>
+                <h1 className="contact-title">
+                    <span>C</span>
+                    <span>o</span>
+                    <span>n</span>
+                    <span>t</span>
+                    <span>a</span>
+                    <span>c</span>
+                    <span>t</span>&nbsp;
+                    <span>M</span>
+                    <span>e</span>
+                </h1>
+                <p className="contact-message">
+                    I'm interested and actively searching for a developer position - I currently reside in New York City, USA but am open to relocation. If you have any other questions or requests, don't hesitate to reach out.
+                </p>
                 <div className="contact-form"></div>
                 <div className="contact-aside"></div>
             </div>
             <div className="contact-right-box">
-                <div className="map-popup"></div>
-                <div className="map-wrapper"></div>
+            <div className="contact-right-cover"></div>
+                <div className="map-popup">
+                    <p>David Venegas</p>
+                    <p>venegasdavidm@gmail.com</p>
+                    <p>1-803-944-1772</p>
+                    <p>NYC, NY - USA</p>
+                </div>
+                <div className="map-wrapper">
+                    <iframe title='Contact Map' src="https://snazzymaps.com/embed/370659" width="100%" height="100%" style={{border: "none"}}/>
+                </div>
             </div>
         </div>
     )
