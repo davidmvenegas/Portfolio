371f54628e383cfb460351cce3d127c653fd57cf
david_venegas <venegasdavidm@gmail.com>
2022-02-09 04:00:33 -0500

    navbar fully responsive

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -9,6 +9,7 @@ import { useState } from 'react'
 function Navbar() {
     const navigate = useNavigate()
     const [navActive, setNavActive] = useState(false)
+    const closeNav = () => setNavActive(false)
     return (
         <Fragment>
             <p className="navbar-toggle-button" onClick={() => setNavActive(!navActive)}>
@@ -23,12 +24,12 @@ function Navbar() {
                     <p>Fullstack Developer</p>
                 </div>
                 <ul className="nav-menu">
-                    <li className="nav-item"><NavLink to="/" activeClassName="active" className="nav-link"><h1 id='first-nav-link'>About</h1></NavLink></li>
-                    <li className="nav-item"><NavLink to="/skills" activeClassName="active" className="nav-link"><h1>My Skills</h1></NavLink></li>
-                    <li className="nav-item"><NavLink to="/work" activeClassName="active" className="nav-link"><h1>Work</h1></NavLink></li>
-                    <li className="nav-item"><NavLink to="/algorithms" activeClassName="active" className="nav-link"><h1>Algorithms</h1></NavLink></li>
-                    <li className="nav-item"><NavLink to="/blogs" activeClassName="active" className="nav-link"><h1>Blogs</h1></NavLink></li>
-                    <li className="nav-item"><NavLink to="/contact" activeClassName="active" className="nav-link"><h1>Contact</h1></NavLink></li>
+                    <li className="nav-item" onClick={() => closeNav()}><NavLink to="/" activeClassName="active" className="nav-link"><h1 id='first-nav-link'>About</h1></NavLink></li>
+                    <li className="nav-item" onClick={() => closeNav()}><NavLink to="/skills" activeClassName="active" className="nav-link"><h1>My Skills</h1></NavLink></li>
+                    <li className="nav-item" onClick={() => closeNav()}><NavLink to="/work" activeClassName="active" className="nav-link"><h1>Work</h1></NavLink></li>
+                    <li className="nav-item" onClick={() => closeNav()}><NavLink to="/algorithms" activeClassName="active" className="nav-link"><h1>Algorithms</h1></NavLink></li>
+                    <li className="nav-item" onClick={() => closeNav()}><NavLink to="/blogs" activeClassName="active" className="nav-link"><h1>Blogs</h1></NavLink></li>
+                    <li className="nav-item" onClick={() => closeNav()}><NavLink to="/contact" activeClassName="active" className="nav-link"><h1>Contact</h1></NavLink></li>
                 </ul>
                 <div className="navbar-bottom">
                     <a href="https://www.linkedin.com/in/david-venegas-m/" target="_blank" rel="noopener noreferrer"><FaLinkedin className='nav-icon'/></a>
