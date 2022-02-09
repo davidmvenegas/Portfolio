25278c198534f3bd7adbcb41632d7bcfe138b5a9
david_venegas <venegasdavidm@gmail.com>
2022-02-09 03:13:56 -0500

    navbar phase 1 resp

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -3,29 +3,39 @@ import { NavLink } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 import { FaGithub, FaLinkedin } from "react-icons/fa"
 import Logo from '../Media/Images/logo.png'
+import { Fragment } from 'react/cjs/react.production.min'
+import { useState } from 'react'
 
 function Navbar() {
     const navigate = useNavigate()
+    const [navActive, setNavActive] = useState(false)
     return (
-        <div className='navbar-container'>
-            <div className="navbar-top">
-                <img src={Logo} alt="DV" onClick={() => navigate('/contact')} />
-                <h1>David Venegas</h1>
-                <p>Fullstack Developer</p>
+        <Fragment>
+            <p className="navbar-toggle-button" onClick={() => setNavActive(!navActive)}>
+                <span className={navActive ? "navbar-line navActive" : "navbar-line"}></span>
+                <span className={navActive ? "navbar-line navActive" : "navbar-line"}></span>
+                <span className={navActive ? "navbar-line navActive" : "navbar-line"}></span>
+            </p>
+            <div className='navbar-container'>
+                <div className="navbar-top">
+                    <img src={Logo} alt="DV" onClick={() => navigate('/contact')} />
+                    <h1>David Venegas</h1>
+                    <p>Fullstack Developer</p>
+                </div>
+                <ul className="nav-menu">
+                    <li className="nav-item"><NavLink to="/" activeClassName="active" className="nav-link"><h1 id='first-nav-link'>About</h1></NavLink></li>
+                    <li className="nav-item"><NavLink to="/skills" activeClassName="active" className="nav-link"><h1>My Skills</h1></NavLink></li>
+                    <li className="nav-item"><NavLink to="/work" activeClassName="active" className="nav-link"><h1>Work</h1></NavLink></li>
+                    <li className="nav-item"><NavLink to="/algorithms" activeClassName="active" className="nav-link"><h1>Algorithms</h1></NavLink></li>
+                    <li className="nav-item"><NavLink to="/blogs" activeClassName="active" className="nav-link"><h1>Blogs</h1></NavLink></li>
+                    <li className="nav-item"><NavLink to="/contact" activeClassName="active" className="nav-link"><h1>Contact</h1></NavLink></li>
+                </ul>
+                <div className="navbar-bottom">
+                    <a href="https://www.linkedin.com/in/david-venegas-m/" target="_blank" rel="noopener noreferrer"><FaLinkedin className='nav-icon'/></a>
+                    <a href="https://github.com/davidmvenegas" target="_blank" rel="noopener noreferrer"><FaGithub className='nav-icon'/></a>
+                </div>
             </div>
-            <ul className="nav-menu">
-                <li className="nav-item"><NavLink to="/" activeClassName="active" className="nav-link"><h1 id='first-nav-link'>About</h1></NavLink></li>
-                <li className="nav-item"><NavLink to="/skills" activeClassName="active" className="nav-link"><h1>My Skills</h1></NavLink></li>
-                <li className="nav-item"><NavLink to="/work" activeClassName="active" className="nav-link"><h1>Work</h1></NavLink></li>
-                <li className="nav-item"><NavLink to="/algorithms" activeClassName="active" className="nav-link"><h1>Algorithms</h1></NavLink></li>
-                <li className="nav-item"><NavLink to="/blogs" activeClassName="active" className="nav-link"><h1>Blogs</h1></NavLink></li>
-                <li className="nav-item"><NavLink to="/contact" activeClassName="active" className="nav-link"><h1>Contact</h1></NavLink></li>
-            </ul>
-            <div className="navbar-bottom">
-                <a href="https://www.linkedin.com/in/david-venegas-m/" target="_blank" rel="noopener noreferrer"><FaLinkedin className='nav-icon'/></a>
-                <a href="https://github.com/davidmvenegas" target="_blank" rel="noopener noreferrer"><FaGithub className='nav-icon'/></a>
-            </div>
-        </div>
+        </Fragment>
     )
 }
 
