70e97b3e5e16ed5e9a0e08f60c7dd9c3705f361b
david_venegas <venegasdavidm@gmail.com>
2022-02-13 14:00:04 -0500

    contact section tweak

--- src/6-Contact/Contact.js
+++ src/6-Contact/Contact.js
@@ -45,7 +45,7 @@ function Contact() {
                     <span id='cLetter_e'>e</span>
                 </h1>
                 <p className="contact-message contactForm1">
-                    I'm available and actively searching for a developer position. I currently reside in New York City, but am open to relocation. If you have any questions or requests, don't hesitate to reach out.
+                    I'm available for a developer position and would love to connect. I currently reside in New York City, but am open to relocation. If you have any questions or requests, don't hesitate to reach out.
                 </p>
                 <form className="contact-form" ref={formRef} onSubmit={sendEmail}>
                     <ul>
