4a08a492c6c391741d8e494a6eac8cfb57081a8b
David Venegas <venegasdavidm@gmail.com>
2024-04-16 18:39:00 -0700

    gh links

--- src/3-Work/Work.js
+++ src/3-Work/Work.js
@@ -30,7 +30,7 @@ import PasswordLogo from '../Media/Images/other-work-icons/PasswordGeneratorLogo
 import NewYearsLogo from '../Media/Images/other-work-icons/NewYearsTimerLogo.png'
 
 function Work() {
-    useEffect(() => {window.scrollTo(0, 0)}, [])
+    useEffect(() => { window.scrollTo(0, 0) }, [])
     const navigate = useNavigate()
     const [port1, setPort1] = useState(false)
     const [port2, setPort2] = useState(false)
@@ -38,24 +38,22 @@ function Work() {
     const [port4, setPort4] = useState(false)
     const [modalID, setModalID] = useState(null)
     const [openModal, setOpenModal] = useState(false)
-    const [otherSepRef, otherSepInView] = useInView({threshold: 0, triggerOnce: true})
-    const [otherTitleRef, otherTitleInView] = useInView({threshold: 0, triggerOnce: true})
-    const [otherDescRef, otherDescInView] = useInView({threshold: 0, triggerOnce: true})
+    const [otherSepRef, otherSepInView] = useInView({ threshold: 0, triggerOnce: true })
+    const [otherTitleRef, otherTitleInView] = useInView({ threshold: 0, triggerOnce: true })
+    const [otherDescRef, otherDescInView] = useInView({ threshold: 0, triggerOnce: true })
     const otherData = [
-        {id: 1, title: 'Weather App', image: WeatherLogo, github: "https://github.com/davidmvenegas/WeatherApp", link: "https://davidmvenegas.github.io/WeatherApp", largeTitle: false},
-        {id: 2, title: 'Favorite Books', image: BooklistLogo, github: "https://github.com/davidmvenegas/MyFavoriteBooks", link: "https://davidmvenegas.github.io/MyFavoriteBooks", largeTitle: false},
-        {id: 3, title: 'Coin Flip', image: CoinFlipLogo, github: "https://github.com/davidmvenegas/CoinFlip", link: "https://davidmvenegas.github.io/CoinFlip", largeTitle: false},
-        {id: 4, title: 'Tic-Tac-Toe', image: TicTacToeLogo, github: "https://github.com/davidmvenegas/TicTacToe", link: "https://davidmvenegas.github.io/TicTacToe", largeTitle: false},
-        {id: 5, title: 'Calculator', image: CalculatorLogo, github: "https://github.com/davidmvenegas/Calculator", link: "https://davidmvenegas.github.io/Calculator", largeTitle: false},
-        {id: 6, title: 'Rock Paper Scissors', image: RockPaperScissorsLogo, github: "https://github.com/davidmvenegas/RockPaperScissors", link: "https://davidmvenegas.github.io/RockPaperScissors", largeTitle: true},
-        {id: 7, title: 'Quiz App', image: QuizLogo, github: "https://github.com/davidmvenegas/quizApp", link: "https://davidmvenegas-quizapp.netlify.app", largeTitle: false},
-        {id: 8, title: 'Starbucks Page', image: StarbucksLogo, github: "https://github.com/davidmvenegas/StarbucksPageClone", link: "https://davidmvenegas.github.io/StarbucksPageClone", largeTitle: false},
-        // BROKEN 👇 THIS ONES BROKENNN
-        {id: 9, title: 'Restaurant Page', image: RestaurantLogo, github: "https://github.com/davidmvenegas/Burgerhouse", link: "https://davidmvenegas.github.io/Burgerhouse", largeTitle: false},
-        {id: 10, title: 'Notes App', image: NotesLogo, github: "https://github.com/davidmvenegas/NotesApp", link: "https://davidmvenegas.github.io/NotesApp", largeTitle: false},
-        {id: 11, title: 'Password Generator', image: PasswordLogo, github: "https://github.com/davidmvenegas/PasswordGenerator", link: "https://davidmvenegas.github.io/PasswordGenerator", largeTitle: true},
-        // FIX FORMATING PLZZZ
-        {id: 12, title: 'New Years Timer', image: NewYearsLogo, github: "https://github.com/davidmvenegas/JS-CountdownTimer", link: "https://davidmvenegas.github.io/JS-CountdownTimer", largeTitle: false},
+        { id: 1, title: 'Weather App', image: WeatherLogo, github: "https://github.com/davidmvenegas/WeatherApp", link: "https://davidmvenegas.github.io/WeatherApp", largeTitle: false },
+        { id: 2, title: 'Favorite Books', image: BooklistLogo, github: "https://github.com/davidmvenegas/MyFavoriteBooks", link: "https://davidmvenegas.github.io/MyFavoriteBooks", largeTitle: false },
+        { id: 3, title: 'Coin Flip', image: CoinFlipLogo, github: "https://github.com/davidmvenegas/CoinFlip", link: "https://davidmvenegas.github.io/CoinFlip", largeTitle: false },
+        { id: 4, title: 'Tic-Tac-Toe', image: TicTacToeLogo, github: "https://github.com/davidmvenegas/TicTacToe", link: "https://davidmvenegas.github.io/TicTacToe", largeTitle: false },
+        { id: 5, title: 'Calculator', image: CalculatorLogo, github: "https://github.com/davidmvenegas/Calculator", link: "https://davidmvenegas.github.io/Calculator", largeTitle: false },
+        { id: 6, title: 'Rock Paper Scissors', image: RockPaperScissorsLogo, github: "https://github.com/davidmvenegas/RockPaperScissors", link: "https://davidmvenegas.github.io/RockPaperScissors", largeTitle: true },
+        { id: 7, title: 'Quiz App', image: QuizLogo, github: "https://github.com/davidmvenegas/quizApp", link: "https://davidmvenegas-quizapp.netlify.app", largeTitle: false },
+        { id: 8, title: 'Starbucks Page', image: StarbucksLogo, github: "https://github.com/davidmvenegas/StarbucksPageClone", link: "https://davidmvenegas.github.io/StarbucksPageClone", largeTitle: false },
+        { id: 9, title: 'Restaurant Page', image: RestaurantLogo, github: "https://github.com/davidmvenegas/Burgerhouse", link: "https://davidmvenegas.github.io/Burgerhouse", largeTitle: false },
+        { id: 10, title: 'Notes App', image: NotesLogo, github: "https://github.com/davidmvenegas/NotesApp", link: "https://davidmvenegas.github.io/NotesApp", largeTitle: false },
+        { id: 11, title: 'Password Generator', image: PasswordLogo, github: "https://github.com/davidmvenegas/PasswordGenerator", link: "https://davidmvenegas.github.io/PasswordGenerator", largeTitle: true },
+        { id: 12, title: 'New Years Timer', image: NewYearsLogo, github: "https://github.com/davidmvenegas/CountdownTimer", link: "https://davidmvenegas.github.io/CountdownTimer", largeTitle: false },
     ]
     function handleOpenModal(id) {
         setModalID(id)
@@ -67,20 +65,20 @@ function Work() {
     }
     return (
         <div className='work-container'>
-            <Modal open={openModal} setOpenModal={setOpenModal} modalID={modalID}/>
+            <Modal open={openModal} setOpenModal={setOpenModal} modalID={modalID} />
             <div className="port-wrapper">
                 <h1 className="port-header">
-                        <span>M</span>
-                        <span>y</span>&nbsp;
-                        <span>P</span>
-                        <span>o</span>
-                        <span>r</span>
-                        <span>t</span>
-                        <span>f</span>
-                        <span>o</span>
-                        <span>l</span>
-                        <span>i</span>
-                        <span>o</span>
+                    <span>M</span>
+                    <span>y</span>&nbsp;
+                    <span>P</span>
+                    <span>o</span>
+                    <span>r</span>
+                    <span>t</span>
+                    <span>f</span>
+                    <span>o</span>
+                    <span>l</span>
+                    <span>i</span>
+                    <span>o</span>
                 </h1>
                 <p className="port-description">
                     <span>A&nbsp;</span>
@@ -101,13 +99,13 @@ function Work() {
                     <div className="port-item" id='portItem1' onMouseEnter={() => setPort1(true)} onMouseLeave={() => setPort1(false)}>
                         <div className="port-gif-box">
                             <div className="port-gif-wrapper">
-                                <img className='port-gif' src={MySimpleWardrobeGif} alt="My Simple Wardrobe Gif" style={port1 ? {opacity: "1"} : {opacity: "0"}}/>
-                                <img className='port-gif port-gif-logo' src={MySimpleWardrobeLogo} alt="My Simple Wardrobe Logo" style={port1 ? {opacity: "0"} : {opacity: "1"}}/>
+                                <img className='port-gif' src={MySimpleWardrobeGif} alt="My Simple Wardrobe Gif" style={port1 ? { opacity: "1" } : { opacity: "0" }} />
+                                <img className='port-gif port-gif-logo' src={MySimpleWardrobeLogo} alt="My Simple Wardrobe Logo" style={port1 ? { opacity: "0" } : { opacity: "1" }} />
                             </div>
                             <a href='https://www.mysimplewardrobe.com/' target="_blank" rel="noreferrer">
                                 <div className="portGifPopup">
                                     <p>Go to site</p>
-                                    <BsBoxArrowUpRight id='goToSitePort'/>
+                                    <BsBoxArrowUpRight id='goToSitePort' />
                                 </div>
                             </a>
                         </div>
@@ -118,7 +116,7 @@ function Work() {
                             <div className="port-btn-box">
                                 <div className='port-btn-more' onClick={() => handleOpenModal(1)}>
                                     <span id='port-more-text'>More Details</span>
-                                    <BsArrowRight id='port-more-arrow'/>
+                                    <BsArrowRight id='port-more-arrow' />
                                 </div>
                             </div>
                         </div>
@@ -126,13 +124,13 @@ function Work() {
                     <div className="port-item" id='portItem2' onMouseEnter={() => setPort2(true)} onMouseLeave={() => setPort2(false)}>
                         <div className="port-gif-box">
                             <div className="port-gif-wrapper">
-                                <img className='port-gif' src={GroceryMagixGif} alt="Grocery Magix Gif" style={port2 ? {opacity: "1"} : {opacity: "0"}}/>
-                                <img className='port-gif port-gif-logo' src={GroceryMagixLogo} alt="Grocery Magix Logo" style={port2 ? {opacity: "0"} : {opacity: "1"}}/>
+                                <img className='port-gif' src={GroceryMagixGif} alt="Grocery Magix Gif" style={port2 ? { opacity: "1" } : { opacity: "0" }} />
+                                <img className='port-gif port-gif-logo' src={GroceryMagixLogo} alt="Grocery Magix Logo" style={port2 ? { opacity: "0" } : { opacity: "1" }} />
                             </div>
                             <a href='https://www.grocerymagix.com/' target="_blank" rel="noreferrer">
                                 <div className="portGifPopup">
                                     <p>Go to site</p>
-                                    <BsBoxArrowUpRight id='goToSitePort'/>
+                                    <BsBoxArrowUpRight id='goToSitePort' />
                                 </div>
                             </a>
                         </div>
@@ -143,7 +141,7 @@ function Work() {
                             <div className="port-btn-box">
                                 <div className='port-btn-more' onClick={() => handleOpenModal(2)}>
                                     <span id='port-more-text'>More Details</span>
-                                    <BsArrowRight id='port-more-arrow'/>
+                                    <BsArrowRight id='port-more-arrow' />
                                 </div>
                             </div>
                         </div>
@@ -151,13 +149,13 @@ function Work() {
                     <div className="port-item" id='portItem3' onMouseEnter={() => setPort3(true)} onMouseLeave={() => setPort3(false)}>
                         <div className="port-gif-box">
                             <div className="port-gif-wrapper">
-                                <img className='port-gif' src={TheFinancialTrackerGif} alt="The Financial Tracker Gif" style={port3 ? {opacity: "1"} : {opacity: "0"}}/>
-                                <img className='port-gif port-gif-logo' src={TheFinancialTrackerLogo} alt="The Financial Tracker Logo" style={port3 ? {opacity: "0"} : {opacity: "1"}}/>
+                                <img className='port-gif' src={TheFinancialTrackerGif} alt="The Financial Tracker Gif" style={port3 ? { opacity: "1" } : { opacity: "0" }} />
+                                <img className='port-gif port-gif-logo' src={TheFinancialTrackerLogo} alt="The Financial Tracker Logo" style={port3 ? { opacity: "0" } : { opacity: "1" }} />
                             </div>
                             <a href='https://www.thefinancialtracker.com/' target="_blank" rel="noreferrer">
                                 <div className="portGifPopup">
                                     <p>Go to site</p>
-                                    <BsBoxArrowUpRight id='goToSitePort'/>
+                                    <BsBoxArrowUpRight id='goToSitePort' />
                                 </div>
                             </a>
                         </div>
@@ -168,7 +166,7 @@ function Work() {
                             <div className="port-btn-box">
                                 <div className='port-btn-more' onClick={() => handleOpenModal(3)}>
                                     <span id='port-more-text'>More Details</span>
-                                    <BsArrowRight id='port-more-arrow'/>
+                                    <BsArrowRight id='port-more-arrow' />
                                 </div>
                             </div>
                         </div>
@@ -176,13 +174,13 @@ function Work() {
                     <div className="port-item" id='portItem4' onMouseEnter={() => setPort4(true)} onMouseLeave={() => setPort4(false)}>
                         <div className="port-gif-box">
                             <div className="port-gif-wrapper">
-                                <img className='port-gif' src={MyCryptomaticGif} alt="Grocery Magix Gif" style={port4 ? {opacity: "1"} : {opacity: "0"}}/>
-                                <img className='port-gif port-gif-logo' src={MyCryptomaticLogo} alt="Grocery Magix Logo" style={port4 ? {opacity: "0"} : {opacity: "1"}}/>
+                                <img className='port-gif' src={MyCryptomaticGif} alt="Grocery Magix Gif" style={port4 ? { opacity: "1" } : { opacity: "0" }} />
+                                <img className='port-gif port-gif-logo' src={MyCryptomaticLogo} alt="Grocery Magix Logo" style={port4 ? { opacity: "0" } : { opacity: "1" }} />
                             </div>
                             <a href='https://www.mycryptomatic.com/' target="_blank" rel="noreferrer">
                                 <div className="portGifPopup">
                                     <p>Go to site</p>
-                                    <BsBoxArrowUpRight id='goToSitePort'/>
+                                    <BsBoxArrowUpRight id='goToSitePort' />
                                 </div>
                             </a>
                         </div>
@@ -193,7 +191,7 @@ function Work() {
                             <div className="port-btn-box">
                                 <div className='port-btn-more' onClick={() => handleOpenModal(4)}>
                                     <span id='port-more-text'>More Details</span>
-                                    <BsArrowRight id='port-more-arrow'/>
+                                    <BsArrowRight id='port-more-arrow' />
                                 </div>
                             </div>
                         </div>
@@ -203,15 +201,15 @@ function Work() {
             <div className="other-separator" ref={otherSepRef} id={otherSepInView ? "education-separator-active" : undefined}></div>
             <div className="other-wrapper">
                 <h1 className="other-header" ref={otherTitleRef} id={otherTitleInView ? "certTitle-active" : undefined}>
-                        <span>O</span>
-                        <span>t</span>
-                        <span>h</span>
-                        <span>e</span>
-                        <span>r</span>&nbsp;
-                        <span>W</span>
-                        <span>o</span>
-                        <span>r</span>
-                        <span>k</span>
+                    <span>O</span>
+                    <span>t</span>
+                    <span>h</span>
+                    <span>e</span>
+                    <span>r</span>&nbsp;
+                    <span>W</span>
+                    <span>o</span>
+                    <span>r</span>
+                    <span>k</span>
                 </h1>
                 <p className="other-description" ref={otherDescRef} id={otherDescInView ? "otherDesc-active" : undefined}>
                     A collection of a few other things I've built. <span>Everything is fully mobile responsive.</span>
